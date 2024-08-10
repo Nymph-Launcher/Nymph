22964ea741afd5d9dca662e9f2be184650cea567
Dawn Chan <dawnchan030920@outlook.com>
2024-08-10 23:06:37 +0800

    [upd] add constraint item view model builder test

--- Nymph.Shared/DependencyInjection/AutofacNymphSharedExtensions.cs
+++ Nymph.Shared/DependencyInjection/AutofacNymphSharedExtensions.cs
@@ -7,14 +7,21 @@ using Nymph.Shared.CandidateItem.List;
 using Nymph.Shared.CandidateItem.Path;
 using Nymph.Shared.CandidateItem.Record;
 using Nymph.Shared.CandidateItem.Unit;
+using Nymph.Shared.ConstraintItem;
+using Nymph.Shared.ConstraintItem.Atom;
+using Nymph.Shared.ConstraintItem.Function;
+using Nymph.Shared.ConstraintItem.List;
+using Nymph.Shared.ConstraintItem.Path;
+using Nymph.Shared.ConstraintItem.Record;
+using Nymph.Shared.ConstraintItem.Unit;
 
 namespace Nymph.Shared.DependencyInjection;
 
 public static class AutofacNymphSharedExtensions
 {
-    public static void RegisterNymphShared(this ContainerBuilder builder)
+    private static void RegisterCandidateItemViewModel(ContainerBuilder builder)
     {
-        // Register rendered candidate item view model
+        // Register candidate item view model
         builder.RegisterGeneric(typeof(AtomCandidateViewModel<>))
             .As(typeof(ICandidateItemViewModel<>))
             .WithParameter(
@@ -58,5 +65,60 @@ public static class AutofacNymphSharedExtensions
         // register candidate item view model builder
         builder.RegisterType<CandidateItemViewModelBuilder>()
             .As<ICandidateItemViewModelBuilder>();
+    }
+
+    private static void RegisterConstraintItemViewModel(ContainerBuilder builder)
+    {
+        // register constraint item view model
+        builder.RegisterGeneric(typeof(AtomConstraintViewModel<>))
+            .As(typeof(IConstraintItemViewModel<>))
+            .WithParameter(
+                (pi, _) => pi.ParameterType.IsGenericType &&
+                           pi.ParameterType.GetGenericTypeDefinition() == typeof(Item.Atom<>),
+                (pi, _) => pi);
+
+        builder.RegisterGeneric(typeof(FunctionConstraintViewModel<,>))
+            .As(typeof(IConstraintItemViewModel<>))
+            .WithParameter(
+                (pi, _) => pi.ParameterType.IsGenericType &&
+                           pi.ParameterType.GetGenericTypeDefinition() == typeof(Item.Function<,>),
+                (pi, _) => pi);
+
+        builder.RegisterGeneric(typeof(ListConstraintViewModel<>))
+            .As(typeof(IConstraintItemViewModel<>))
+            .WithParameter(
+                (pi, _) => pi.ParameterType.IsGenericType &&
+                           pi.ParameterType.GetGenericTypeDefinition() == typeof(Item.List<>),
+                (pi, _) => pi);
+
+        builder.RegisterGeneric(typeof(PathConstraintViewModel<,>))
+            .As(typeof(IConstraintItemViewModel<>))
+            .WithParameter(
+                (pi, _) => pi.ParameterType.IsGenericType &&
+                           pi.ParameterType.GetGenericTypeDefinition() == typeof(Item.Path<,>),
+                (pi, _) => pi);
+
+        builder.RegisterType(typeof(RecordConstraintViewModel))
+            .As(typeof(IConstraintItemViewModel<Item.Record>))
+            .WithParameter(
+                (pi, _) => pi.ParameterType == typeof(Item.Record),
+                (pi, _) => pi);
+
+        builder.RegisterType(typeof(UnitConstraintViewModel))
+            .As(typeof(IConstraintItemViewModel<Item.Unit>))
+            .WithParameter(
+                (pi, _) => pi.ParameterType == typeof(Item.Unit),
+                (pi, _) => pi);
+
+        // register constraint item view model builder
+        builder.RegisterType<ConstraintItemViewModelBuilder>()
+            .As<IConstraintItemViewModelBuilder>();
+    }
+
+    public static void RegisterNymphShared(this ContainerBuilder builder)
+    {
+        RegisterCandidateItemViewModel(builder);
+
+        RegisterConstraintItemViewModel(builder);
     }
 }
