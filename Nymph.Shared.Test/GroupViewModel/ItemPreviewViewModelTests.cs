f6dd9288c7bcd7ab6c1becdd95e0b14377cb6ad6
Dawn Chan <dawnchan030920@outlook.com>
2024-08-14 22:57:50 +0800

    [upd] add ItemPreviewViewModelTests.cs

---
+++ Nymph.Shared.Test/GroupViewModel/ItemPreviewViewModelTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Nymph.Shared.Group;
+
+namespace Nymph.Shared.Test.GroupViewModel;
+
+public class ItemPreviewViewModelTests
+{
+    private readonly IContainer _container;
+
+    public ItemPreviewViewModelTests()
+    {
+        var builder = new ContainerBuilder();
+
+        builder.RegisterNymphShared();
+
+        _container = builder.Build();
+    }
+
+    [Fact]
+    public void ItemPreviewViewModel_Emits_UnitItem()
+    {
+        var item = new Item.Atom<int>(10);
+
+        var itemPreviewGroup = new Model.Group.ItemPreview<Item.Atom<int>>(item);
+
+        var itemPreviewViewModel =
+            _container.Resolve<IGroupViewModel<Model.Group.ItemPreview<Item.Atom<int>>>>(
+                new TypedParameter(typeof(Model.Group.ItemPreview<Item.Atom<int>>), itemPreviewGroup));
+
+        itemPreviewViewModel.CandidateItemViewModels[0].Item.Should().BeOfType(typeof(Item.Unit));
+    }
+}
