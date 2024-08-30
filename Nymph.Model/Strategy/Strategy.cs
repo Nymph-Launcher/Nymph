a4ec6e2ec1e858ef25c83be84da6a55c3e892bf9
Dawn Chan <dawnchan030920@outlook.com>
2024-08-30 23:03:12 +0800

    [upd] add strategy and handler, middleware folders

---
+++ Nymph.Model/Strategy/Strategy.cs
@@ -0,0 +1,17 @@
+﻿namespace Nymph.Model.Strategy;
+
+/// <summary>
+///     The strategy to search groups.
+/// </summary>
+public class Strategy : IStrategy
+{
+    /// <summary>
+    ///     Search groups by the state.
+    /// </summary>
+    /// <param name="state">State.</param>
+    /// <returns>Groups.</returns>
+    public Seq<Group> Search(State state)
+    {
+        throw new NotImplementedException();
+    }
+}
